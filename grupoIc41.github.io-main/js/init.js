96b3353b2da1cb808e7d8c089b3f863833e38396
Luis Ledesma <88347870+1ui5s@users.noreply.github.com>
2021-08-02 16:11:27 -0500

    Update init.js

--- grupoIc41.github.io-main/js/init.js
+++ grupoIc41.github.io-main/js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyCfMMR4bFjJgfECWOsx6iRCHPZTeDQ9GBQ",
-    authDomain: "bdfalumnos41.firebaseapp.com",
-    projectId: "bdfalumnos41",
-    storageBucket: "bdfalumnos41.appspot.com",
-    messagingSenderId: "463512573259",
-    appId: "1:463512573259:web:2531082c01b911490b04ce",
-    measurementId: "G-DXY3ZYMKY4"
+      apiKey: "AIzaSyDmhlaNhML3f_OMQOhLqfYOB8pVerBxVII",
+  authDomain: "tareafinal1.firebaseapp.com",
+  projectId: "tareafinal1",
+  storageBucket: "tareafinal1.appspot.com",
+  messagingSenderId: "753464591206",
+  appId: "1:753464591206:web:009390e317b75d8f78fea7",
+  measurementId: "G-ECPP428JQ9"
 });
